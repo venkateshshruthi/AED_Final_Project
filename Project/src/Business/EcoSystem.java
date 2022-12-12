08143f1df87d32d299c049079495d25997c30238
venkateshshruthi <113217203+venkateshshruthi@users.noreply.github.com>
2022-12-11 22:33:23 -0500

    testing smtp integration

---
+++ Project/src/Business/EcoSystem.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import Business.Enterprise.Enterprise;
+import Business.Network.Network;
+import Business.Organization.Organization;
+import Business.Role.Role;
+import Business.Role.SystemAdminRole;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Shruthi Venkatesh
+ */
+public class EcoSystem extends Organization {
+
+    private static EcoSystem business;
+    private ArrayList<Network> networkList;
+
+    public static EcoSystem getInstance() {
+        if (business == null) {
+            business = new EcoSystem();
+        }
+        return business;
+    }
+
+    public Network createAndAddNetwork() {
+        Network network = new Network();
+        networkList.add(network);
+        return network;
+    }
+    
+    public void removeNetwork(Network network) {
+        networkList.remove(network);
+    }
+    
+    public void updateNetwork(Network network,String name){
+        for(Network n:networkList){
+            if(n.equals(network)){
+                n.setName(name);
+            }
+        }
+    }
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roleList = new ArrayList<Role>();
+        roleList.add(new SystemAdminRole());
+        return roleList;
+    }
+
+    private EcoSystem() {
+        super(null);
+        networkList = new ArrayList<Network>();
+    }
+
+    public ArrayList<Network> getNetworkList() {
+        return networkList;
+    }
+
+    public void setNetworkList(ArrayList<Network> networkList) {
+        this.networkList = networkList;
+    }
+
+    public boolean checkIfUserIsUnique(String userName) {
+        if (this.getUserAccountDirectory().checkIfUsernameIsUnique(userName)) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isDuplicateNetwork(String name) {
+     for(Network n:networkList){
+            if(n.equals(name)){
+                return true;
+            }
+        }    
+    return false;
+    }
+}
